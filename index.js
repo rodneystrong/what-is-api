77691505a79795aa090f5cdd9730a98339e8b712
alex kang <akang2@gmail.com>
2018-05-21 05:17:51 -0700

    first API request

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
 //use onload() to access the JSON data 
 request.onload = function () {
+  var data = JSON.parse(this.response);
 
+  data.forEach(movie => {
+    console.log(movie.title);
+  });
 }
 
 //once done, send the request using send()
