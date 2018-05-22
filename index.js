89e94990008503de8c5088d821a0ba9370f7cbd9
alex kang <akang2@gmail.com>
2018-05-21 17:45:59 -0700

    used fetch, got correct response

--- index.js
+++ index.js
@@ -12,29 +12,38 @@ var request = new XMLHttpRequest();
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
 //use onload() to access the JSON data 
-request.onload = function () {
-  var data = JSON.parse(this.response);
+// request.onload = function () {
+//   var data = JSON.parse(this.response);
 
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      console.log(movie.title);
-      //container element
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
+//   if (request.status >= 200 && request.status < 400) {
+//     data.forEach(movie => {
+//       console.log(movie.title);
+//       //container element
+//       const card = document.createElement('div');
+//       card.setAttribute('class', 'card');
 
-      //actual element holding content
-      const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+//       //actual element holding content
+//       const h1 = document.createElement('h1');
+//       h1.textContent = movie.title;
 
-      //put the element inside our container var
-      container.appendChild(card);
-      card.appendChild(h1);
-    });
-  } else {
-    console.log('error');
-  }
+//       //put the element inside our container var
+//       container.appendChild(card);
+//       card.appendChild(h1);
+//     });
+//   } else {
+//     console.log('error');
+//   }
 
-}
+// }
+
+//using the fetch API
+fetch('https://ghibliapi.herokuapp.com/films')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (myJson) {
+    console.log(myJson[0].title);
+  });
 
 //once done, send the request using send()
 request.send();
